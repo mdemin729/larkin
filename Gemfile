db859bbe9df64339c7e36627b90d68909037c084
Maxim Demin <m-s-demin@yandex.ru>
2016-07-27 16:02:16 +0400

    delete client

--- Gemfile
+++ Gemfile
@@ -7,6 +7,13 @@ gem 'rails', '4.2.5.1'
 gem 'pg', '~> 0.15'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
+# Use CoffeeScript for .coffee assets and views
+gem 'coffee-rails', '~> 4.1.0'
+
+# Use jquery as the JavaScript library
+gem 'jquery-rails'
+# Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
+gem 'turbolinks'
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -8,8 +8,42 @@ class ClientsController < ApplicationController
   end
 
   def new
+    @client = Client.new
   end
 
   def edit
+    @client = Client.find(params[:id])
+  end
+
+  def create
+    @client = Client.new(client_params)
+
+    if @client.save
+      redirect_to @client
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @client = Client.find(params[:id])
+
+    if @client.update(client_params)
+      redirect_to @client
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @client = Client.find(params[:id])
+    @client.destroy
+
+    redirect_to clients_path
+  end
+
+  private
+  def client_params
+    params.require(:client).permit(:name, :address, :city, :state, :zip, :phone_number, :phone_ext)
   end
 end
