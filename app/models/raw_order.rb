5b54b3d78bf7349d754d58af9b48c1fd47be6729
Maxim Demin <m-s-demin@yandex.ru>
2016-07-31 12:18:15 +0400

    validate fields of raw orders

--- app/models/raw_order.rb
+++ app/models/raw_order.rb
@@ -8,7 +8,11 @@ class RawOrder < ActiveRecord::Base
   validates_presence_of :origin_name, :origin_raw_line_1, :origin_city, :origin_state, :origin_zip, :origin_country
   validates_presence_of :client_name, :destination_raw_line_1, :destination_city, :destination_state, :destination_zip, :destination_country
 
-  validates :origin_zip, :destination_zip, format: {with: /\d{5}/}
-  validates :phone_number, format: {with: /(\d.)?\d{3}.\d{3}.\d{4}( x.*)?/, message: 'wrong phone number format'}
-  validates :purchase_order_number, format: {with: /\d{9}/}
+  validates :origin_country, :destination_country, inclusion: {in: %w(US)}
+
+  validates :origin_zip, :destination_zip, format: {with: /\A\d{5}\z/}
+  validates :phone_number, format: {with: /\A(\d-)?.?\d{3}.\d{3}.\d{4}( x\d*)?\z/, message: 'wrong phone number format'}
+  validates :purchase_order_number, format: {with: /\A\d{9}.*\z/}
+  validates :volume, numericality: {less_than_or_equal_to: 1400}
+  validates :handling_unit_quantity, numericality: {greater_than: 0}
 end
