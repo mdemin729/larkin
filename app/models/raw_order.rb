c03398eca10216deb28f85b5136bb6432ac9b0eb
Maxim Demin <m-s-demin@yandex.ru>
2016-08-01 21:58:14 +0400

    raw order validations

--- app/models/raw_order.rb
+++ app/models/raw_order.rb
@@ -6,7 +6,7 @@ class RawOrder < ActiveRecord::Base
     begin
       delivery_date = Date.strptime raw_order.delivery_date, '%m/%d/%Y'
     rescue ArgumentError
-      errors[:delivery_date] << 'Delivery date is invalid'
+      errors[:delivery_date] << 'must be valid date in MM/DD/YYYY format'
     end
   end
 
@@ -16,9 +16,15 @@ class RawOrder < ActiveRecord::Base
   validates_presence_of :origin_name, :origin_raw_line_1, :origin_city, :origin_state, :origin_zip, :origin_country
   validates_presence_of :client_name, :destination_raw_line_1, :destination_city, :destination_state, :destination_zip, :destination_country
 
-  validates :origin_country, :destination_country, inclusion: {in: %w(US)}
+  validates :origin_country, :destination_country, inclusion: {in: %w(US)}, allow_blank: true
+  validates :handling_unit_type, inclusion: {in: %w(box)}
+  validates :origin_state, :destination_state,
+            inclusion: {
+                in: %w(AL	AK	AZ	AR	CA	CO	CT	DE	FL	GA	HI	ID	IL	IN	IA	KS	KY	LA	ME	MD	MA	MI	MN	MS	MO	MT	NE	NV	NH	NJ	NM	NY	NC	ND	OH	OK	OR	PA	RI	SC	SD	TN	TX	UT	VT	VA	WA	WV	WI	WY),
+                message: 'must be US state abbreviation'
+            }, allow_blank: true
+  validates :origin_zip, :destination_zip, format: {with: /\A\d{5}\z/}, allow_blank: true
 
-  validates :origin_zip, :destination_zip, format: {with: /\A\d{5}\z/}
   validates :phone_number, format: {with: /\A(\d-)?.?\d{3}.\d{3}.\d{4}( x\d*)?\z/, message: 'wrong phone number format'}
   validates :purchase_order_number, format: {with: /\A\d{9}.*\z/}
   validates :volume, numericality: {less_than_or_equal_to: 1400}
